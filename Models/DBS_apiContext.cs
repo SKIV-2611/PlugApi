ed67c577d1a198a981532f750fbf896d9c5f505e
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-24 14:03:22 +0300

    Добавьте файлы проекта.

---
+++ Models/DBS_apiContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PlugApi.Models
+{
+    public partial class DBS_apiContext : DbContext
+    {
+        public DBS_apiContext()
+        {
+        }
+
+        public DBS_apiContext(DbContextOptions<DBS_apiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PaymentOrder> PaymentOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=DBS_gridContext-6a8df6d3-4faa-4384-9f7e-5aa4364bc234;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/PaymentOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace PlugApi.Models
+{
+    [Table("PaymentOrder")]
+    public partial class PaymentOrder
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        public string SenderAccount { get; set; }
+        [Required]
+        public string ReceiverAccount { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Sum { get; set; }
+        [Required]
+        public string Reference { get; set; }
+        public int Status { get; set; }
+    }
+}
