50e2580e99d51fda5481569979a43dbdef7d1b73
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-24 14:47:35 +0300

    Some changes

--- Models/DBS_apiContext.cs
+++ Models/DBS_apiContext.cs
@@ -23,8 +23,7 @@ namespace PlugApi.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=DBS_gridContext-6a8df6d3-4faa-4384-9f7e-5aa4364bc234;Trusted_Connection=True;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer();
             }
         }
 
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PlugApi.Models;
 
 namespace PlugApi
 {
@@ -26,7 +20,7 @@ namespace PlugApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DBS_apiContext>(op => op.UseSqlServer(Configuration.GetConnectionString("DBS_apiContext")));
             services.AddControllers();
             //services.AddSwaggerGen(c =>
             //{
